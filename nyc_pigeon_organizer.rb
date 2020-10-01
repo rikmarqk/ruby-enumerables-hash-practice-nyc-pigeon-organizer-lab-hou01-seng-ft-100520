46263db57ba99dc664014c5dce3b9d68713369df
Ricardo Marquez <rikmarqk@gmail.com>
2020-10-01 07:28:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  names = {}
+  data.each do |attributes, categ|
+    categ.each do |specs, indiv|
+      indiv.each do |identity|
+        if names[identity] == nil
+          names[identity] = {}
+        end
+        if names[identity][attributes] == nil
+          names[identity][attributes] = []
+        end
+        names[identity][attributes].push(specs.to_s)
+      end
+    end 
+  end
+  names
 end
